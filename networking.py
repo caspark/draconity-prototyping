4677006254fac72051237f8c6d3b6f7f9b4d969d
Caspar Krieger <git@asparck.com>
2019-03-29 22:42:34 -0700

    remove unused networking functions

--- networking.py
+++ networking.py
@@ -7,33 +7,6 @@ from ring_buffer import RingBuffer
 MSG_HEADER_FMT = "!QQ"  # transaction_id, data_length
 
 
-def recv_msg(sock):
-    transaction_id, length = struct.unpack(
-        MSG_HEADER_FMT, _recv_raw(sock, struct.calcsize(MSG_HEADER_FMT))
-    )
-    message_bytes = _recv_raw(sock, length)
-    return transaction_id, bson.loads(message_bytes)
-
-
-def _recv_raw(sock, message_length):
-    data = bytearray(message_length)
-    offset = 0
-    while offset < message_length:
-        chunk = sock.recv(min(message_length - offset, 2048))
-        if chunk == b"":
-            raise RuntimeError("socket connection broken")
-        new_offset = offset + len(chunk)
-        data[offset:new_offset] = chunk
-        offset = new_offset
-    return data
-
-
-def send_msg(sock, transaction_id, obj):
-    data = bson.dumps(obj)
-    send_raw(sock, struct.pack(MSG_HEADER_FMT, transaction_id, len(data)))
-    send_raw(sock, data)
-
-
 def send_raw(sock, data):
     message_length = len(data)
     bytes_sent_total = 0
